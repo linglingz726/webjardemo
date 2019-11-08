a3dcb753069ab0a4a810ab470a2ea47bfa8141a1
zero <3511339505@qq.com>
2019-11-08 17:02:20 +0800

    Initial commit

---
+++ src/main/java/com/offcn/webjardemo/WebjardemoApplication.java
@@ -0,0 +1,13 @@
+package com.offcn.webjardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebjardemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebjardemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/offcn/webjardemo/WebjardemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offcn.webjardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebjardemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
